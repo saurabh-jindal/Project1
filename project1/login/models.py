e993481f678f6fd6fb5f2bb185f083fb69bda36e
Saurabh Jindal <jindalsaurabh533@gmail.com>
2019-09-10 17:27:05 +0530

    say to first

---
+++ project1/login/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+class SignUp(models.Model):
+    name = models.CharField(max_length=70)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(max_length=70,blank=True, null= True, unique= True)
+
+
+    def __str__(self):
+        return self.name
+
