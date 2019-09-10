7442767a6e75de5a7d1b6ab285e83d3305be830b
Saurabh Jindal <jindalsaurabh533@gmail.com>
2019-09-10 18:04:52 +0530

    just added the code

--- project1/login/views.py
+++ project1/login/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from login.models import *
+from django.contrib.auth.models import User
 
 
 
 # Create your views here.
 def index(request):
     pass
+def delete(request,username):
+    try:
+        u = User.objects.get(username = username)
+        u.delete()
+        messages.sucess(request, "The user is deleted")
+    except:
+      messages.error(request, "The user not found")    
+    return render(request, 'home.html')
+
--- project1/project1/urls.py
+++ project1/project1/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.views.generic.base import TemplateView
+from login import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'), 
-    path('', include('login.urls')),
+    path('<username>', views.delete, name="delete"),
 ]
 
